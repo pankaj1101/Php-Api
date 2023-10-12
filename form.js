ed8acbf5457074ed57008d470fae84aaa28dde11
Pankaj Ram <116742441+pankaj1101@users.noreply.github.com>
2023-10-12 22:35:11 +0530

    Create form.js

---
+++ form.js
@@ -0,0 +1,68 @@
+'use client';
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: formData.name,
+      age: formData.age,
+    };
+
+    const apiUrl = 'http://localhost/api/insert.php';
+    axios
+      .post(apiUrl, data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then((response) => {
+        console.log('Data successfully posted:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Age:</label>
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+
+  );
+}
