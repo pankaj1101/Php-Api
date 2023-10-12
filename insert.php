495a802e7a51822ac5ad5038e1b39005a51f80ee
Pankaj Ram <116742441+pankaj1101@users.noreply.github.com>
2023-10-12 22:45:01 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 require_once("./db/connection.php");
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
